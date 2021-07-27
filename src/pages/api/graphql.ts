bf10f2c4a5f414bb60681d799a68cccfe96498e9
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2021-07-27 19:07:33 +0200

    Bump packages, migrate to apollo server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,29 +1,56 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro"
+import { NextApiHandler } from "next"
 
-import { typeDefs } from "../../graphql/schema";
-import { fetchMarkets, getMarketData } from "../../lib/marketData";
+import { typeDefs } from "../../graphql/schema"
+import { fetchMarkets, getMarketData } from "../../lib/marketData"
+import cors from "micro-cors"
 
 const resolvers = {
   Query: {
     bestMarket: async (_parent, { btcAmount }) => {
-      const marketList = await fetchMarkets(btcAmount);
-      return getMarketData({ btcAmount, marketList });
+      const marketList = await fetchMarkets(btcAmount)
+      return getMarketData({ btcAmount, marketList })
     },
   },
-};
+}
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   context() {
-    return {};
+    return {}
   },
-});
+})
+
+let apolloServerHandler: NextApiHandler
+
+async function getApolloServerHandler() {
+  if (!apolloServerHandler) {
+    await apolloServer.start()
+
+    apolloServerHandler = apolloServer.createHandler({
+      path: "/api/graphql",
+    })
+  }
+
+  return apolloServerHandler
+}
+
+const handler: NextApiHandler = async (req, res) => {
+  const apolloServerHandler = await getApolloServerHandler()
+
+  if (req.method === "OPTIONS") {
+    res.end()
+    return
+  }
+
+  return apolloServerHandler(req, res)
+}
 
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+export default cors()(handler)
